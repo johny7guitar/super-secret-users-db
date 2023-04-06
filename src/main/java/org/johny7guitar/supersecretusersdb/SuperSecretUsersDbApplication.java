a2ba493767fc6accd7679886c5561b0c9c3fa270
johny7guitar <johny7guitar@protonmail.com>
2023-04-06 18:05:11 +0000

    init commit

---
+++ src/main/java/org/johny7guitar/supersecretusersdb/SuperSecretUsersDbApplication.java
@@ -0,0 +1,13 @@
+package org.johny7guitar.supersecretusersdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperSecretUsersDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperSecretUsersDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/johny7guitar/supersecretusersdb/SuperSecretUsersDbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.johny7guitar.supersecretusersdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperSecretUsersDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
