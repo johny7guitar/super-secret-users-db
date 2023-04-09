f3a89103ba151226061b7d0e68e5a5d5917baeb8
johny7guitar <johny7guitar@protonmail.com>
2023-04-09 11:40:42 +0000

    add update user status feature to api

--- src/main/java/org/johny7guitar/supersecretusersdb/web/UserController.java
+++ src/main/java/org/johny7guitar/supersecretusersdb/web/UserController.java
@@ -1,10 +1,12 @@
 package org.johny7guitar.supersecretusersdb.web;
 
+import org.johny7guitar.supersecretusersdb.entities.UserStatus;
 import org.johny7guitar.supersecretusersdb.repository.UserRepository;
 import org.johny7guitar.supersecretusersdb.services.UserService;
 import org.johny7guitar.supersecretusersdb.util.UserMapper;
 import org.johny7guitar.supersecretusersdb.web.hal.UserIdOnlyModel;
 import org.johny7guitar.supersecretusersdb.web.hal.UserModel;
+import org.johny7guitar.supersecretusersdb.web.hal.UserStatusChangeModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
@@ -45,6 +47,24 @@ public class UserController{
                         ),
                 id),
                 HttpStatus.OK);
+    }
+
+    @PatchMapping(path="/{id}/update_status/{new_status}")
+    public HttpEntity<UserStatusChangeModel> updateUserStatus(
+            @PathVariable("id") long id,
+            @PathVariable("new_status") UserStatus newStatus
+    ){
+        UserStatusChangeDto statusChange = userService.updateUserStatus(id, newStatus);
+
+        return new ResponseEntity<>(
+                new UserStatusChangeModel(
+                        statusChange.getUserId(),
+                        statusChange.getOldUserStatus(),
+                        statusChange.getNewUserStatus()
+                ),
+                HttpStatus.OK
+        );
+
     }
 
 }
